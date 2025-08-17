4da1a80fc5e36951550dd8d85861771f800a367b
amrit-boro <email.amrit.boro@gmail.com>
2025-08-17 11:12:44 +0530

    nothing

--- server/router/sellerRouter.js
+++ server/router/sellerRouter.js
@@ -5,6 +5,11 @@ const router = express.Router();
 
 router.post("/signup", authController.sellersignup);
 router.post("/login", authController.sellerLogin);
+router.patch(
+  "/updateSeller",
+  authController.SellerProtect,
+  sellerController.updataSeller
+);
 
 router
   .route("/")
@@ -13,9 +18,6 @@ router
   .route("/seller-within/:distance/center/:latlng/unit/:unit")
   .get(sellerController.getSellerWithin);
 
-router
-  .route("/:id")
-  .get(sellerController.getSeller)
-  .patch(sellerController.updataSeller);
+router.route("/:id").get(sellerController.getSeller);
 
 module.exports = router;
